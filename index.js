b4059a07ea99b3a5cf70f375548140f8d1fc0d02
Alen1945 <dwialendraipangselly23@gmail.com>
2020-04-12 00:49:46 +0700

    function create realtime chat

--- index.js
+++ index.js
@@ -1,46 +1,142 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
-admin.initializeApp()
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-exports.OnCreateUser = functions.auth.user().onCreate(async (user)=>{
-    const resultCreate = await admin.firestore().collection('Users').doc(user.uid).set({
+exports.OnCreateUser = functions.auth.user().onCreate(async (user) => {
+  const resultCreate = await admin
+    .firestore()
+    .collection('Users')
+    .doc(user.uid)
+    .set({
       displayName: user.displayName || '',
-      email: user.email ||'',
+      email: user.email || '',
       emailVerified: user.emailVerified || false,
       isAnonymous: user.isAnonymous || false,
       metadata: {
-        creationTime: new admin.firestore.Timestamp(new Date(user.metadata.creationTime).getTime()/1000,0),
-        lastSignInTime: new admin.firestore.Timestamp(new Date(user.metadata.lastSignInTime).getTime()/1000,0),
+        creationTime: new admin.firestore.Timestamp(
+          new Date(user.metadata.creationTime).getTime() / 1000,
+          0,
+        ),
+        lastSignInTime: new admin.firestore.Timestamp(
+          new Date(user.metadata.lastSignInTime).getTime() / 1000,
+          0,
+        ),
       },
       phoneNumber: user.phoneNumber,
       photoURL: user.photoURL || '',
       isOnline: true,
-    })
-    return resultCreate
-})
+    });
+  return resultCreate;
+});
 
-exports.updateUser= functions.https.onCall(async (dataUser,context)=>{
-  if(!context.auth) {
-    throw new functions.https.HttpsError('unauthenticated',"You must login first")
+exports.updateUser = functions.https.onCall(async (dataUser, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'You must login first',
+    );
   }
-  const resultUpdate = await admin.firestore().collection('Users').doc(context.auth.uid).update(dataUser)
-  if(resultUpdate){
-    return (await admin.firestore().collection('Users').doc(context.auth.uid).get()).data()
+  const resultUpdate = await admin
+    .firestore()
+    .collection('Users')
+    .doc(context.auth.uid)
+    .update(dataUser);
+  if (resultUpdate) {
+    return (
+      await admin.firestore().collection('Users').doc(context.auth.uid).get()
+    ).data();
   } else {
-    console.log(resultUpdate)
-    throw new functions.https.HttpsError('aborted',"Something Wrong")
+    console.log(resultUpdate);
+    throw new functions.https.HttpsError('aborted', 'Something Wrong');
   }
-})
+});
 
-exports.userSignOut= functions.https.onCall(async (data, context)=>{
-  if(!context.auth){
-    throw new functions.https.HttpsError('unauthenticated',"You Not SignIn")
+exports.userSignOut = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'You Not SignIn');
   }
-  return await admin.firestore().collection('Users').doc(context.auth.uid).update({
-    isOnline:false
-  })
-})
+  return await admin
+    .firestore()
+    .collection('Users')
+    .doc(context.auth.uid)
+    .update({
+      isOnline: false,
+    });
+});
 
-exports.onDeleteUser = functions.auth.user().onDelete(async user=>{
-  return await admin.firestore().collection('Users').doc(user.uid).delete()
-})
+exports.onDeleteUser = functions.auth.user().onDelete(async (user) => {
+  return await admin.firestore().collection('Users').doc(user.uid).delete();
+});
+
+exports.getOrCreateRoom = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'You Not SignIn');
+  }
+  const getRoom = await admin
+    .firestore()
+    .collection('RoomChat')
+    .where(`member.${context.auth.uid}`, '==', true)
+    .where(`member.${data.secondUser}`, '==', true)
+    .where('type', '==', 'DIRECT_MESSAGE')
+    .get();
+  if (getRoom.empty) {
+    const createRoom = await admin
+      .firestore()
+      .collection('RoomChat')
+      .add({
+        type: 'DIRECT_MESSAGE',
+        member: {
+          [context.auth.uid]: true,
+          [data.secondUser]: true,
+        },
+      });
+    await admin
+      .firestore()
+      .collection('Users')
+      .doc(`${context.auth.uid}`)
+      .collection('ListChat')
+      .add({
+        id: createRoom.id,
+      });
+    await admin
+      .firestore()
+      .collection('Users')
+      .doc(`${data.secondUser}`)
+      .collection('ListChat')
+      .add({
+        id: createRoom.id,
+      });
+    return createRoom.id;
+  } else {
+    let id = '';
+    getRoom.docs.forEach((doc) => {
+      id = doc.id;
+    });
+    return id;
+  }
+});
+
+exports.createMessage = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'You Not SignIn');
+  }
+  admin
+    .firestore()
+    .collection('RoomChat')
+    .doc(`${data.idRoom}`)
+    .update({
+      lastMessage: data.text,
+      lastAddedMessage: new Date(data.createdAt).toUTCString(),
+    });
+  await admin
+    .firestore()
+    .collection('RoomMessage')
+    .doc(`${data.idRoom}`)
+    .collection('messages')
+    .add({
+      user: context.auth.uid,
+      text: data.text,
+      createdAt: new Date(data.createdAt).toUTCString(),
+    });
+  return true;
+});
